bf3f098a3069d240252d9ac3fc9bfbea0c3391b9
John Breiner <jbreiner@gmail.com>
2016-12-22 04:59:00 -0600

    Added Database connection

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+//connect to MySQL
+$con = mysqli_connect("localhost", "root", "", "shoutit");
+
+//Test connection
+
+if(mysqli_connect_errno()){
+	echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+}
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+include 'database.php' ;
+//create Select Query
+$query = "SELECT * FROM shouts";
+$shouts = mysqli_query($con, $query);
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,12 +20,9 @@
 			</header>
 			<div id="shouts">
 				<ul>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
-					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to?</li>
+					<?php while($row = mysqli_fetch_assoc($shouts)) : ?>
+						<li class="shout"><span><?php echo $row['time'] ?> - </span><strong><?php echo $row['user'] ?>:</strong> <?php echo $row['message'] ?></li>
+					<?php  endwhile ?>
 				</ul>
 			</div>
 			<div id="input">
